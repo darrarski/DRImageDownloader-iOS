d9ed98b816e51c3bd0176aa69ef19d17f11eb8f0
Dariusz Rybicki <darrarski@gmail.com>
2015-04-01 01:37:06 +0200

    DRImageDownloaderTest - added tests for different fetch policy methods, uses mocked up image downloader

--- DRImageDownloaderExampleTests/DRImageDownloaderTests.m
+++ DRImageDownloaderExampleTests/DRImageDownloaderTests.m
@@ -5,11 +5,11 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
-#import "DRImageDownloader.h"
+#import "DRImageDownloaderMock.h"
 
 @interface DRImageDownloaderTests : XCTestCase
 
-@property (nonatomic, strong) DRImageDownloader *imageDownloader;
+@property (nonatomic, strong) DRImageDownloaderMock *imageDownloader;
 
 @end
 
@@ -18,7 +18,7 @@
 - (void)setUp
 {
     [super setUp];
-    self.imageDownloader = [[DRImageDownloader alloc] init];
+    self.imageDownloader = [[DRImageDownloaderMock alloc] init];
 }
 
 - (void)tearDown
@@ -27,7 +27,7 @@
     self.imageDownloader = nil;
 }
 
-- (void)testSetMemoryCacheSize
+- (void)test_setMemoryCacheSize
 {
     XCTAssertEqual(self.imageDownloader.memoryCacheSize, DRImageDownloaderDefaultMemoryCacheSize);
     self.imageDownloader.memoryCacheSize = 512;
@@ -36,12 +36,73 @@
     XCTAssertEqual(self.imageDownloader.memoryCacheSize, 1024);
 }
 
-- (void)testSetUseSharedCache
+- (void)test_setUseSharedCache
 {
     self.imageDownloader.useSharedCache = YES;
     XCTAssertEqual(self.imageDownloader.isUsingSharedCache, YES);
     self.imageDownloader.useSharedCache = NO;
     XCTAssertEqual(self.imageDownloader.isUsingSharedCache, NO);
+}
+
+- (void)test_getImageFromNetwork
+{
+    UIImage *imageMock = [UIImage new];
+    self.imageDownloader.mockedImageFromNetwork = imageMock;
+    [self.imageDownloader getImageWithUrl:[NSURL new] loadCompletion:^(UIImage *image) {
+        XCTAssertEqualObjects(image, imageMock);
+    }];
+}
+
+- (void)test_getImageFromCache
+{
+    UIImage *imageMock = [UIImage new];
+    self.imageDownloader.mockedImageFromCache = imageMock;
+    [self.imageDownloader getImageWithUrl:[NSURL new] fromCacheCompletion:^(UIImage *image) {
+        XCTAssertEqualObjects(image, imageMock);
+    }];
+}
+
+- (void)test_getImageFromCacheThenLoad
+{
+    UIImage *cachedImageMock = [UIImage new];
+    UIImage *loadedImageMock = [UIImage new];
+    self.imageDownloader.mockedImageFromCache = cachedImageMock;
+    self.imageDownloader.mockedImageFromNetwork = loadedImageMock;
+    __block NSUInteger completionBlockRunCount = 0;
+    [self.imageDownloader getImageWithUrl:[NSURL new] fromCacheThenLoadCompletion:^(UIImage *image) {
+        completionBlockRunCount++;
+        if (completionBlockRunCount == 1) {
+            XCTAssertEqual(image, cachedImageMock);
+        }
+        else if (completionBlockRunCount == 2) {
+            XCTAssertEqual(image, loadedImageMock);
+        }
+    }];
+    XCTAssertEqual(completionBlockRunCount, 2);
+}
+
+- (void)test_getImageFromCacheElseLoad_withEmptyCache
+{
+    UIImage *loadedImageMock = [UIImage new];
+    self.imageDownloader.mockedImageFromNetwork = loadedImageMock;
+    __block NSUInteger completionBlockRunCount = 0;
+    [self.imageDownloader getImageWithUrl:[NSURL new] fromCacheElseLoadCompletion:^(UIImage *image) {
+        completionBlockRunCount++;
+        XCTAssertEqual(image, loadedImageMock);
+    }];
+    XCTAssertEqual(completionBlockRunCount, 1);
+}
+
+- (void)test_getImageFromCacheElseLoad_withNonEmptyCache
+{
+    UIImage *cachedImageMock = [UIImage new];
+    self.imageDownloader.mockedImageFromCache = cachedImageMock;
+    __block NSUInteger completionBlockRunCount = 0;
+    [self.imageDownloader getImageWithUrl:[NSURL new] fromCacheElseLoadCompletion:^(UIImage *image) {
+        completionBlockRunCount++;
+        XCTAssertEqual(image, cachedImageMock);
+    }];
+    XCTAssertEqual(completionBlockRunCount, 1);
 }
 
 @end
