262e96c0f2670304e7e1ad183a4053f37d294a4d
Dariusz Rybicki <darrarski@gmail.com>
2015-04-01 00:16:34 +0200

    ViewController - example code, downloading random image on tap

--- DRImageDownloaderExample/UI/ViewController.m
+++ DRImageDownloaderExample/UI/ViewController.m
@@ -4,11 +4,51 @@
 //
 
 #import "ViewController.h"
+#import "DRImageDownloader.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
 @implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapOnView:)]];
+}
+
+- (void)didTapOnView:(id)sender
+{
+    [self reloadImage];
+}
+
+- (void)reloadImage
+{
+    __weak typeof(self) welf = self;
+    [[DRImageDownloader sharedInstance] getImageWithUrl:[self getRandomImageUrlWithSize:[self getImageSizeForImageView]]
+                                             completion:^(UIImage *image) {
+                                                 dispatch_async(dispatch_get_main_queue(), ^{
+                                                     welf.imageView.image = image;
+                                                 });
+                                             }];
+}
+
+- (NSURL *)getRandomImageUrlWithSize:(CGSize)imageSize
+{
+    return [[NSURL alloc] initWithString:[NSString stringWithFormat:@"http://lorempixel.com/%ld/%ld", (long)imageSize.width, (long)imageSize.height]];
+}
+
+- (CGSize)getImageSizeForImageView
+{
+    CGFloat screenScale = [[UIScreen mainScreen] scale];
+    CGSize imageSize = CGSizeApplyAffineTransform(
+        self.imageView.frame.size,
+        CGAffineTransformMakeScale(screenScale, screenScale)
+    );
+    return imageSize;
+}
 
 @end
